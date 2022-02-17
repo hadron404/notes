bc3d1d1a393aae4cda8dbc2295b5d379e5e17d42
zhouqiang <nqws1002@163.com>
2022-02-17 17:13:37 +0800

    Refactoring: 运用多态：将switch语句放在合适的位置
    合理的使用switch语句

--- src/Movie.java
+++ src/Movie.java
@@ -4,7 +4,7 @@
  * @author zhouqiang
  * @date 2022/2/17
  */
- class Movie {
+class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
@@ -28,5 +28,32 @@
 
     public void setPriceCode(int priceCode) {
         this._priceCode = priceCode;
+    }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE)
+                && daysRented > 1) return 2;
+        else
+            return 1;
     }
 }
